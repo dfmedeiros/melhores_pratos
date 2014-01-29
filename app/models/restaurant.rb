70a901522c80ec994f44f9fecc7068787cb82145
Diego Medeiros <diegoo.fm@gmail.com>
2014-01-29 20:52:21 -0300

    adding validation to restaurant form

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,13 @@ class Restaurant < ActiveRecord::Base
   belongs_to :city
   belongs_to :user
 
+  validates :name, presence: true
+  validates :slug, presence: true, uniqueness: true
+  validates :zip_code, presence: true
+  validates :street, presence: true
+  validates :street_number, presence: true
+  validates :neighborhood, presence: true
+  validates :city, presence: true
+  validates :phone_number, presence: true
+
 end
