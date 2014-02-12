ffa911a3157a3f0658af1e20074ba3af93f70776
Diego Medeiros <diegoo.fm@gmail.com>
2014-02-11 23:03:46 -0300

    changing user avatar on settings page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_record_not_found
 
   def rescue_record_not_found
     flash[:error] = "Page not found"
     redirect_to request.env["HTTP_REFERER"].present? ? :back : root_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) << [:name, :avatar]
   end
 end
