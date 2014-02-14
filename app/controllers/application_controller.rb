9ea0f2d1f83f269d07ed7834c0462bd7097cab84
Diego Medeiros <diegoo.fm@gmail.com>
2014-02-13 21:03:46 -0300

    started to add strings to locale file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,11 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_record_not_found
 
   def rescue_record_not_found
-    flash[:error] = "Page not found"
-    redirect_to request.env["HTTP_REFERER"].present? ? :back : root_path
+    redirect_to root_path, flash: { error: I18n.t('app.page_not_found') }
   end
 
   def configure_permitted_parameters
