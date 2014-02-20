a1e1a41a9e703c8968c896c4a0e229aedba1b512
Diego Medeiros <diegoo.fm@gmail.com>
2014-02-19 21:04:18 -0300

    formating website and adding specs to decorator files

--- app/decorators/restaurant_decorator.rb
+++ app/decorators/restaurant_decorator.rb
@@ -4,4 +4,10 @@ class RestaurantDecorator < Draper::Decorator
   def formatted_address
     "#{street}, #{neighborhood}, #{city.name}, #{state.acronym}"
   end
+
+  def formatted_website
+    h.link_to website do
+      "<i class=\"icon-globe\"></i>#{website}".html_safe
+    end
+  end
 end
--- spec/decorators/restaurant_decorator_spec.rb
+++ spec/decorators/restaurant_decorator_spec.rb
@@ -2,8 +2,24 @@ require "spec_helper"
 
 describe RestaurantDecorator do
 
-  describe "#formatted_address" do
+  subject { restaurant.decorate }
 
+  describe "#formatted_address" do
+    let(:state) { build_stubbed(:state, acronym: "XX") }
+    let(:city) { build_stubbed(:city, name: "Test City", state: state) }
+    let(:restaurant) { build_stubbed(:restaurant, city: city) }
+
+    it "returns address with city and state" do
+      expect(subject.formatted_address).to eq "Street Name, Neighborhood, Test City, XX"
+    end
+  end
+
+  describe "#formatted_website" do
+    let(:restaurant) { build_stubbed(:restaurant, website: "http://example.com") }
+
+    it "returns link to website formatted" do
+      expect(subject.formatted_website).to eq "<a href=\"http://example.com\"><i class=\"icon-globe\"></i>http://example.com</a>"
+    end
   end
 
 end
